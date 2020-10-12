15376dfc4046f949dc2a66ac03fb20c2e85eb7de
Sabine <sabine.bertram@mailbox.org>
2020-10-12 17:11:28 +0200

    fix: allow for odd number of characters to be encrypted

--- index.js
+++ index.js
@@ -70,13 +70,13 @@ function decode(buf) {
 }
 
 function ab2str(buf) {
-  return String.fromCharCode.apply(null, new Uint16Array(buf));
+  return String.fromCharCode.apply(null, new Uint8Array(buf))
 }
 function str2ab(str) {
-  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-  var bufView = new Uint16Array(buf);
+  var buf = new ArrayBuffer(str.length)
+  var bufView = new Uint8Array(buf)
   for (var i=0, strLen=str.length; i < strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
+    bufView[i] = str.charCodeAt(i)
   }
   return buf;
 }
